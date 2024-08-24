5912f9852ef1c6bf1908366ddd6e82358fc56f7f
rKaushalya <ravinduk56@gmail.com>
2024-08-24 13:14:23 +0530

    scroll top button added

--- src/view/common/NavBar/NavBar.tsx
+++ src/view/common/NavBar/NavBar.tsx
@@ -1,16 +1,24 @@
 import * as React from 'react';
 import Logo from './../../../assest/images/cute-mobile-phone-character-saying-thank-you-vector-41712253.jpg';
+import {FaArrowUp} from 'react-icons/fa';
 
 export const NavBar: React.FC = () => {
     const [isScrolled, setIsScrolled] = React.useState(false);
+    const [showScrollButton, setShowScrollButton] = React.useState(false);
 
     const handleScroll = () => {
         const offset = window.scrollY;
-        if (offset > 50) { // Adjust the value to control when the background changes
+        if (offset > 50) {
             setIsScrolled(true);
+            setShowScrollButton(true);
         } else {
             setIsScrolled(false);
+            setShowScrollButton(false);
         }
+    };
+
+    const scrollToTop = () => {
+        window.scrollTo({ top: 0, behavior: 'smooth' });
     };
 
     React.useEffect(() => {
@@ -28,37 +36,45 @@ export const NavBar: React.FC = () => {
     };
 
     return (
-        <div
-            className={`flex h-14 flex-row items-center justify-around fixed w-screen z-20 font-publicSans transition-colors duration-300 ${isScrolled ? 'bg-white border-b-2' : 'bg-transparent'}`}>
-            <img className="h-10 w-10" src={Logo} alt="Logo" />
+        <>
+            <div
+                className={`flex h-14 flex-row items-center justify-around fixed w-screen z-20 font-publicSans transition-colors duration-300 ${isScrolled ? 'bg-white border-b-2' : 'bg-transparent'}`}>
+                <img className="h-10 w-10" src={Logo} alt="Logo" />
 
-            <div>
-                <ul className="list-none flex mt-1">
-                    <li className="relative mr-5 hover:text-secondary">
-                        <button onClick={() => handleScrollTo('introduction')}>Home</button>
-                    </li>
-
-                    <li className="relative mr-5 hover:text-secondary">
-                        <button onClick={() => handleScrollTo('about')}>About</button>
-                    </li>
-
-                    <li className="relative mr-5 hover:text-secondary">
-                        <button onClick={() => handleScrollTo('education')}>Education</button>
-                    </li>
-
-                    <li className="relative mr-5 hover:text-secondary">
-                        <button onClick={() => handleScrollTo('skills')}>Tech Stack</button>
-                    </li>
-
-                    <li className="relative mr-5 hover:text-secondary">
-                        <button onClick={() => handleScrollTo('projects')}>Project</button>
-                    </li>
-
-                    <li className="relative mr-5 hover:text-secondary">
-                        <button onClick={() => handleScrollTo('contact')}>Contact</button>
-                    </li>
-                </ul>
+                <div>
+                    <ul className="list-none flex mt-1">
+                        <li className="relative mr-5 hover:text-purple-700">
+                            <button onClick={() => handleScrollTo('introduction')}>Home</button>
+                        </li>
+                        <li className="relative mr-5 hover:text-purple-700">
+                            <button onClick={() => handleScrollTo('about')}>About</button>
+                        </li>
+                        <li className="relative mr-5 hover:text-purple-700">
+                            <button onClick={() => handleScrollTo('education')}>Education</button>
+                        </li>
+                        <li className="relative mr-5 hover:text-purple-700">
+                            <button onClick={() => handleScrollTo('skills')}>Tech Stack</button>
+                        </li>
+                        <li className="relative mr-5 hover:text-purple-700">
+                            <button onClick={() => handleScrollTo('projects')}>Project</button>
+                        </li>
+                        <li className="relative mr-5 hover:text-purple-700">
+                            <button onClick={() => handleScrollTo('contact')}>Contact</button>
+                        </li>
+                    </ul>
+                </div>
             </div>
-        </div>
+
+            {/* Scroll to Top Button */}
+            {showScrollButton && (
+                <button
+                    className="fixed bottom-10 right-10 bg-purple-700 text-white px-3 py-3 rounded-full shadow-lg
+                    transition-transform duration-300 ease-in-out hover:bg-blue-700 animate-bounce"
+                    onClick={scrollToTop}
+                >
+                    <FaArrowUp/>
+                </button>
+            )}
+        </>
     );
 };
