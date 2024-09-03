2f5da9ba940899cef2cc65a919b9c6b7225b85ba
rKaushalya <ravinduk56@gmail.com>
2024-09-03 11:55:03 +0530

    particles bug fix

---
+++ src/component/ParticlesBackground/ParticlesBackground.js
@@ -0,0 +1,56 @@
+import React, { useEffect } from 'react';
+
+const ParticlesBackground = () => {
+    useEffect(() => {
+        if (window.particlesJS) {
+            window.particlesJS('particles-js', {
+                particles: {
+                    number: {
+                        value: 70,
+                        density: {
+                            enable: true,
+                            value_area: 800,
+                        },
+                    },
+                    color: { value: "#000000" },
+                    shape: {
+                        type: 'circle',
+                    },
+                    opacity: {
+                        value: 0.5,
+                    },
+                    size: {
+                        value: 3,
+                    },
+                    line_linked: {
+                        enable: true,
+                        distance: 150,
+                        color: '#000000',
+                        opacity: 0.4,
+                        width: 1,
+                    },
+                    move: {
+                        enable: true,
+                        speed: 5,
+                    },
+                },
+                interactivity: {
+                    detect_on: 'canvas',
+                    events: {
+                        onhover: { enable: true, mode: 'repulse' },
+                        onclick: { enable: true, mode: 'push' },
+                    },
+                    modes: {
+                        repulse: { distance: 200, duration: 0.4 },
+                        push: { particles_nb: 4 },
+                    },
+                },
+                retina_detect: true,
+            });
+        }
+    }, []);
+
+    return <div id="particles-js" className="absolute w-full h-full"></div>;
+};
+
+export default ParticlesBackground;
--- src/view/pages/Introduction/Introduction.tsx
+++ src/view/pages/Introduction/Introduction.tsx
@@ -1,10 +1,9 @@
 import React from 'react';
 import {ReactTyped as Typed} from 'react-typed';
-import Particles from "react-tsparticles";
 import {loadFull} from 'tsparticles';
 import profilePic from './../../../assest/images/profile/about2.jpg';
-import './particles.css';
 import {FaArrowDown} from 'react-icons/fa';
+import ParticlesBackground from "../../../component/ParticlesBackground/ParticlesBackground";
 
 export class Introduction extends React.Component {
     particlesInit = async (main: any) => {
@@ -25,77 +24,7 @@ export class Introduction extends React.Component {
                 className="relative min-h-screen bg-gray-50 flex sm:flex-col sm:justify-center sm:items-center md:flex-row md:justify-around px-4 md:px-8"
             >
                 {/* Particles background */}
-                <Particles
-                    id="tsparticles"
-                    options={{
-                        fpsLimit: 120,
-                        interactivity: {
-                            events: {
-                                onClick: {
-                                    enable: true,
-                                    mode: "push",
-                                },
-                                onHover: {
-                                    enable: true,
-                                    mode: "repulse",
-                                },
-                                resize: true,
-                            },
-                            modes: {
-                                push: {
-                                    quantity: 4,
-                                },
-                                repulse: {
-                                    distance: 200,
-                                    duration: 0.4,
-                                },
-                            },
-                        },
-                        particles: {
-                            color: {
-                                value: "#000000",
-                            },
-                            links: {
-                                color: "#000000",
-                                distance: 150,
-                                enable: true,
-                                opacity: 0.4,
-                                width: 1,
-                            },
-                            collisions: {
-                                enable: true,
-                            },
-                            move: {
-                                direction: "none",
-                                enable: true,
-                                outModes: {
-                                    default: "bounce",
-                                },
-                                random: false,
-                                speed: 2.2,
-                                straight: false,
-                            },
-                            number: {
-                                density: {
-                                    enable: true,
-                                    area: 800,
-                                },
-                                value: 50,
-                            },
-                            opacity: {
-                                value: 0.5,
-                            },
-                            shape: {
-                                type: "circle",
-                            },
-                            size: {
-                                value: {min: 1, max: 5},
-                            },
-                        },
-                        detectRetina: true,
-                    }}
-                    className="absolute"
-                />
+                <ParticlesBackground />
 
                 {/* Introduction content */}
                 <div
